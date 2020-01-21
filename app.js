109bc5edd55f89dfcd2d78845f3d243f270034bb
jamie <charja113@gmail.com>
2020-01-21 15:25:09 -0600

    linted app.js and main-front.js add in code to be able to delete messages moved around a few funtion declerations

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-//setting the stuff we need for node (still need to fix deps)
+// setting the stuff we need for node (still need to fix deps)
 const express = require('express');
+
 const app = express();
 const {
-  Pool
+  Pool,
 } = require('pg');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+
 app.use(express.static('public'));
 
-//set our resources and pages
+// set our resources and pages
 app.get('/', (req, res) => {
   res.render('index.ejs');
 });
 app.use('/css', express.static(`${__dirname}/css`));
-app.use('/resources', express.static(`${__dirname}/resources`))
+app.use('/resources', express.static(`${__dirname}/resources`));
 
 const pool = new Pool({
   user: 'postgres',
@@ -27,102 +29,111 @@ function timestuffs(timepsql) {
   const then = Date.parse(timepsql);
   const now = Date.now();
   const diff = now - then;
-  const _seconds = diff * 0.001;
-  const seconds = _seconds % 60;
-  const minutes = _seconds / 60 % 60;
-  const hours = _seconds / 3600 % 24;
-  const days = _seconds / 86400 % 365;
-  const years = _seconds / 31536000;
-  const text = ['years', 'days', 'hours', 'minutes', 'seconds'];
-  //console.log([years, days, hours, minutes, seconds].map((n, i) => Math.floor(n) + ' ' + text[i]).join(', '));
-  //console.log(minutes)
+  const mseconds = diff * 0.001;
+  const years = mseconds / 31536000;
+  const minutes = (years * 365 * 24 * 60);
+  // const testtime = (years * 365*24*60);
+  // console.log(testtime);
   return minutes;
 }
 
 function onNewUuid(uuidInfo) {
-  const text = 'update users set uuid = $3, last_login = now() where username = $1 and password = $2'
+  const text = 'update users set uuid = $3, last_login = now() where username = $1 and password = $2';
   pool.query(text, uuidInfo, (err, res) => {
     if (err) {
       console.log(err);
-      //console.log(uuidInfo)
-    } else(
-      console.log(res, uuidInfo, 'new signin and uuid')
-    )
-  })
+      // console.log(uuidInfo)
+    } else {
+      (
+        console.log(res, uuidInfo, 'new signin and uuid')
+      );
+    }
+  });
 }
 
 function getNotes(username) {
-  let selection = 'select * from saved_notes where username = $1 order by noteid desc'
+  const selection = 'select * from saved_notes where username = $1 order by noteid desc';
   pool.query(selection, [username], (err, res) => {
     if (err) {
-      console.log(err)
+      console.log(err);
     } else {
-      console.log('sending notes for ' + username);
-      let notesend = [res, username]
-      io.emit('noteSend', notesend)
+      console.log(`sending notes for ${username}`);
+      const notesend = [res, username];
+      io.emit('noteSend', notesend);
     }
-  })
+  });
 }
+
+function deleteNote(theNote) {
+  const selection = 'select last_login, username from users where uuid=$1;';
+  pool.query(selection, [theNote[1]], (err, res) => {
+    console.log(`note${theNote[0]} deleted by ${res.rows[0].username}`);
+    const lastcheck = timestuffs(res.rows[0].last_login);
+    if (lastcheck < 30) {
+      // console.log(`Note ${theNote[0]} Deleted`)
+      pool.query('Delete from saved_notes where noteid=$1', [theNote[0]]);
+    } else {
+      console.log('Either bad UUID or session is timed out');
+    }
+  });
+}
+
 
 function onuuidauth(uuid) {
   console.log('uuid auth', uuid);
-  let call = 'select * from users where uuid= $1'
+  const call = 'select * from users where uuid= $1';
   pool.query(call, [uuid], (err, res) => {
     if (err) {
-      console.log(err)
+      console.log(err);
     } else {
-      let lasttime = timestuffs(res.rows[0].last_login)
-      console.log(lasttime)
+      const lasttime = timestuffs(res.rows[0].last_login);
+      console.log(lasttime);
       if (lasttime < 30) {
-        let sendinfo = [true, res.rows[0].username, true]
+        const sendinfo = [true, res.rows[0].username, true];
         pool.query('update users set last_login = now() where uuid = $1', [uuid]);
-        io.emit('authEmit', sendinfo)
+        io.emit('authEmit', sendinfo);
       }
     }
-  })
-
+  });
 }
 
 function newnote(note) {
   console.log('new note recived');
-  let usernameQuery = 'select username, last_login from users where uuid=$1'
+  const usernameQuery = 'select username, last_login from users where uuid=$1';
   pool.query(usernameQuery, [note[3]], (err, res) => {
     if (err) {
       console.error(err);
     } else {
-      let username01 = res.rows[0].username
-      console.log(note)
-      console.log(res)
-      let lastcheck = timestuffs(res.rows[0].last_login)
+      const username01 = res.rows[0].username;
+      //console.log(note);
+      //console.log(res);
+      const lastcheck = timestuffs(res.rows[0].last_login);
       if (lastcheck < 30) {
-        let addNoteQuery = `insert into saved_notes (username, note, created, checked, connection) values ($1, $2, $4, 'f', $3)`
-        console.log('seems to have worked', addNoteQuery, [username01, note[0], note[1], note[2]] )
-        pool.query(addNoteQuery, [username01, note[0], note[1], note[2]])
-      }
-      else{
-        console.warn('user has not been active in the last 30 minutes and has attempted to save a new comment')
+        const addNoteQuery = 'insert into saved_notes (username, note, created, checked, connection) values ($1, $2, $4, \'f\', $3)';
+        //console.log('seems to have worked', addNoteQuery, [username01, note[0], note[1], note[2]]);
+        pool.query(addNoteQuery, [username01, note[0], note[1], note[2]]);
+      } else {
+        console.warn('user has not been active in the last 30 minutes and has attempted to save a new comment');
       }
     }
-  })
+  });
 }
 
 function onsignin(signinfo) {
-  console.log('sign in attempt ' + signinfo)
-  const text = 'select * from users where password=$2 and username=$1'
+  console.log(`sign in attempt ${signinfo}`);
+  const text = 'select * from users where password=$2 and username=$1';
   pool.query(text, signinfo, (err, res) => {
     if (err) {
       console.log(err);
+    } else if (res.rowCount !== 0) {
+      console.log(res);
+      const emitinf = [true, signinfo[0]];
+      console.log(emitinf);
+      io.emit('authEmit', emitinf);
     } else {
-      if (res.rowCount != 0) {
-        console.log(res);
-        let emitinf = [true, signinfo[0]]
-        console.log(emitinf)
-        io.emit('authEmit', emitinf)
-      } else {
-        io.emit('authEmit', [false, signinfo[0]])
-      }
+      io.emit('authEmit', [false, signinfo[0]]);
     }
-  })
+  });
 }
 
 io.sockets.on('connection', (socket) => {
@@ -134,7 +145,8 @@ io.sockets.on('connection', (socket) => {
   socket.on('uuidauth', onuuidauth);
   socket.on('newuuid', onNewUuid);
   socket.on('getNotes', getNotes);
-  socket.on('newNote', newnote)
+  socket.on('newNote', newnote);
+  socket.on('deletenote', deleteNote);
 });
 
 
--- resources/main-front.js
+++ resources/main-front.js
@@ -1,116 +1,138 @@
-//make some vars here so they dont come back as not existing
+// make some vars here so they dont come back as not existing
 let info;
-//this is gonna check if we have a uuid cookie and if we do were gonna ask the server if its still valid
-if ($.cookie('uuid')) {
-  var socket = io.connect('localhost:8089');
-  socket.emit('uuidauth', $.cookie('uuid'))
-  console.log($.cookie('uuid'))
-  auth()
+
+// setup our uuid generator
+function create_UUID() {
+  let dt = new Date().getTime();
+  const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+    const r = (dt + Math.random() * 16) % 16 | 0;
+    dt = Math.floor(dt / 16);
+    return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+  });
+  $.cookie('uuid', uuid);
+  return uuid;
 }
 
-//this is gonna handle the login once the button is clicked
-function login() {
-  event.preventDefault()
-  info = [$('#userName').val(), $('#password').val()]
-  var socket = io.connect('localhost:8089');
-  console.log('button clicked')
-  console.log(info)
-  socket.emit('signIn', info);
-  auth()
-  return info;
-}
-//this is going to handle auth for both uuid and username auth, we are gonna see if we have a null info which means it was uuid
-//if its null we are gonna set the info to the uuid, we are gonna get back the auth status true or false, the uuid to make sure its the right person getting it
-//and i cant for the life of me remember why i have a second true coming back after the uuid coming back... oh yea, its to say its uuid XD
+// Go ahead and setup the auth function
 function auth() {
   let socket = io.connect('localhost:8089');
-  socket.on('authEmit', function (conInfo) {
-    if (info == null) {
+  socket.on('authEmit', (conInfo) => {
+    if (info === null) {
       info = [$.cookie('uuid')];
     }
-    if ((conInfo[0] === true && info[0] == conInfo[1]) || (conInfo[0] === true && conInfo[2] === true)) {
-      console.log(conInfo)
-      $('#logContainer').remove()
-      if (conInfo[2] != true) {
-        let uuid = create_UUID();
-        let uuidInfo = [info[0], info[1], uuid]
-        console.log(uuidInfo)
+    if ((conInfo[0] === true && info[0] === conInfo[1]) || (conInfo[0] === true && conInfo[2] === true)) {
+      console.log(conInfo);
+      $('#logContainer').remove();
+      if (conInfo[2] !== true) {
+        const uuid = create_UUID();
+        const uuidInfo = [info[0], info[1], uuid];
+        console.log(uuidInfo);
         socket.emit('newuuid', uuidInfo);
       }
-      socket.emit('getNotes', conInfo[1])
+      socket.emit('getNotes', conInfo[1]);
       socket.on('noteSend', loadnotes);
-    } else if (conInfo[0] === false && info[0] == conInfo[1]) {
+    } else if (conInfo[0] === false && info[0] === conInfo[1]) {
       socket = null;
       $('<p style="color: red;" >Incorrect Username or Password</p>').insertBefore('#userName').delay(10000)
         .fadeOut(100);
     } else {
-      socket = null
-      console.log('else hit, check send')
+      socket = null;
+      console.log('else hit, check send');
     }
-  })
-}
-
-function create_UUID() {
-  var dt = new Date().getTime();
-  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-    var r = (dt + Math.random() * 16) % 16 | 0;
-    dt = Math.floor(dt / 16);
-    return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
   });
-  $.cookie('uuid', uuid)
-  return uuid;
 }
 
-function newcomment(){
-  //event.preventDefault();
-  var datetime = new Date().toLocaleString();
-  let sendinfo = [$('#newNote').val(), $('#topic').val(), datetime, $.cookie('uuid')]
-  console.log(sendinfo)
-  socket.emit('newNote', sendinfo);
+// this is gonna handle the login once the button is clicked
+function login() {
+  event.preventDefault();
+  info = [$('#userName').val(), $('#password').val()];
+  const socket = io.connect('localhost:8089');
+  console.log('button clicked');
+  console.log(info);
+  socket.emit('signIn', info);
+  auth();
+  return info;
+}
+// this is going to handle auth for both uuid and username auth, we are gonna see if we have a
+// null info which means it was uuid
+// if its null we are gonna set the info to the uuid, we are gonna get back the auth status true or
+// false, the uuid to make sure its the right person getting it
+// and i cant for the life of me remember why i have a second true coming back after
+// the uuid coming back... oh yea, its to say its uuid XD
+
+function deleteNote(elem) {
+  console.log(`${elem.srcElement.dataset.indexNumber}`);
+  const conf = true;
+  if (conf === true) {
+    const deleteobj = [`${elem.srcElement.dataset.indexNumber}`, $.cookie('uuid')];
+    socket.emit('deletenote', deleteobj);
+    console.log(deleteobj);
+  } else {
+    console.log('command not deleted');
   }
-
-
-function stopedTyping(){
-  if ($(this).val() == '' || $('#topic').val() == '') {
-    $('#saving').attr('disabled', true);
-} else {
-    $('#saving').prop('disabled', false);
 }
+
+function newcomment() {
+  // event.preventDefault();
+  const datetime = new Date().toLocaleString();
+  const sendinfo = [$('#newNote').val(), $('#topic').val(), datetime, $.cookie('uuid')];
+  console.log(sendinfo);
+  socket.emit('newNote', sendinfo);
+}
+
+
+function stopedTyping() {
+  if ($(this).val() === '' || $('#topic').val() === '') {
+    $('#saving').attr('disabled', true);
+  } else {
+    $('#saving').prop('disabled', false);
+  }
 }
 
 function loadnotes(notes) {
-  //let note = notes[1].rows[0].note
+  // let note = notes[1].rows[0].note
   console.log(notes);
   const $loggedInAs = (
-    $(`<p id="name" style='display: inline-block;' class='w3-pannel w3-blue w3-card-4 w3-padding w3-teal'>`)
-    .data('name', notes[0].rows[0].username)
-    .text(`Logged in as ${notes[0].rows[0].username}`)
+    $('<p id="name" style=\'display: inline-block;\' class=\'w3-pannel w3-blue w3-card-4 w3-padding w3-teal\'>')
+      .data('name', notes[0].rows[0].username)
+      .text(`Logged in as ${notes[0].rows[0].username}`)
   );
-  $('#main').append(`<div id='loggedin'></div>`)
-  $('#loggedin').append($loggedInAs)
-  $('#main').append(`<br>`)
-  $('#main').append(`<div id='notesDiv' class='notesDiv'></div>`)
-  $('#notesDiv').append(`<table id='notes' class = 'w3-border w3-teal' class="notesTable"> </table>`)
-  $('#notes').append(`<tr id='noteIds' class="idrows"><td id='idlabel' class="idrows">ID</td></tr>`)
-  $('#notes').append(`<tr id='noteTimes' class="timerows"><td id="timeLabel" class="timerows">Time</td></tr>`)
-  $('#notes').append(`<tr id='noteItself' class="noterows"><td id="noteITself" class="noterows">Note</td></tr>`)
-  notes[0].rows.forEach(element => {
+  $('#main').append('<div id=\'loggedin\'></div>');
+  $('#loggedin').append($loggedInAs);
+  $('#main').append('<br>');
+  $('#main').append('<form id=\'notesDiv\' class=\'notesDiv\'></div>');
+  $('#notesDiv').append('<table id=\'notes\' class = \'w3-border w3-teal\' class="notesTable"> </table>');
+  $('#notes').append('<tr id=\'noteIds\' class="idrows"><td id=\'idlabel\' class="idrows">ID</td></tr>');
+  $('#notes').append('<tr id=\'noteTimes\' class="timerows"><td id="timeLabel" class="timerows">Time</td></tr>');
+  $('#notes').append('<tr id=\'noteItself\' class="noterows"><td id="noteITself" class="noterows">Note</td></tr>');
+  $('#notes').append('<tr id=\'noteDelete\' class=\'delete\'><td id="noteDeleteRow" class=deleteBtn>Delete</td></tr>');
+  notes[0].rows.forEach((element) => {
     console.log(element);
-    $('#noteIds').append(`<td class='w3-padding w3-column idrows' id=${element.noteid}>${element.noteid}</td>`)
-    $('#noteTimes').append(`<td class='w3-padding w3-column timerows' id='time${element.created}'>${element.created}</td>`)
-    $('#noteItself').append(`<td class='w3-padding noterows' id='noteItself${element.note}'><div class='setshit'>${element.note}</div></td>`)
+    $('#noteIds').append(`<td class='w3-padding w3-column idrows' id=${element.noteid}>${element.noteid}</td>`);
+    $('#noteTimes').append(`<td class='w3-padding w3-column timerows' id='time${element.created}'>${element.created}</td>`);
+    $('#noteItself').append(`<td class='w3-padding noterows' id='noteItself${element.note}'><div class='setshit'>${element.note}</div></td>`);
+    $('#noteDelete').append(`<td class='w3-padding noterows' id ='delete${element.noteid}'><button data-index-number ='${element.noteid}' id='dltBtn${element.noteid}'>delete</button></td>`);
+    const deleting = document.getElementById(`dltBtn${element.noteid}`);
+    deleting.addEventListener('click', deleteNote);
   });
-  $('#main').append(`<br><form id='note-taking' class='w3-container w3-light-grey'</form>`)
-  $('#note-taking').append(`<label>topic/case</lable>`)
-  $('#note-taking').append(`<input style='height:50%' class='w3-input w3-padding w3-border w3-large-round' id='topic' type='text'></input>`)
-  $('#note-taking').append(`<label>note</lable>`)
-  $('#note-taking').append(`<textarea style='height: 50%' rows='6' class='w3-input w3-padding w3-border w3-large-round' id='newNote' type='text'></textarea>`)
-  $('#note-taking').append(`<button disabled='true' id='saving' class='w3-button w3-border w3-large-round'>save</button>`)
+  $('#main').append('<br><form id=\'note-taking\' class=\'w3-container w3-light-grey\'</form>');
+  $('#note-taking').append('<label>topic/case</lable>');
+  $('#note-taking').append('<input style=\'height:50%\' class=\'w3-input w3-padding w3-border w3-large-round\' id=\'topic\' type=\'text\'></input>');
+  $('#note-taking').append('<label>note</lable>');
+  $('#note-taking').append('<textarea style=\'height: 50%\' rows=\'6\' class=\'w3-input w3-padding w3-border w3-large-round\' id=\'newNote\' type=\'text\'></textarea>');
+  $('#note-taking').append('<button disabled=\'true\' id=\'saving\' class=\'w3-button w3-border w3-large-round\'>save</button>');
   $('#newNote').keydown(stopedTyping);
-  $('#saving').click(newcomment)
+  $('#saving').click(newcomment);
 }
 
-//this is just the click event, not sure where i wanna put it
-$(document).ready(function () {
-  $('#login').click(login)
-})
+// this is just the click event, not sure where i wanna put it
+$(document).ready(() => {
+  $('#login').click(login);
+  // this is gonna check if we have a uuid cookie and if we do were gonna ask the server if its still valid
+  if ($.cookie('uuid')) {
+    const socket = io.connect('localhost:8089');
+    socket.emit('uuidauth', $.cookie('uuid'));
+    console.log($.cookie('uuid'));
+    auth();
+  }
+});
